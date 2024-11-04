4d81d7cab715e007775cd684e9adfcba4d0c7f38
Brian Faust <brian@faust.de.com>
2024-11-04 13:30:45 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ src/Json.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BaseCodeOy\Payload;
+
+final class Json extends AbstractNormalizer
+{
+    public function encode(mixed $contents): string
+    {
+        return \json_encode($contents);
+    }
+
+    public function decode(mixed $contents): array
+    {
+        return \json_decode($contents, true, \JSON_THROW_ON_ERROR);
+    }
+}
---
+++ src/Xml.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BaseCodeOy\Payload;
+
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+final class Xml extends AbstractNormalizer
+{
+    public function encode(mixed $contents): string
+    {
+        return (new Serializer(
+            [new ObjectNormalizer()],
+            [new XmlEncoder()],
+        ))->serialize($contents, 'xml');
+    }
+
+    public function decode(mixed $contents): array
+    {
+        return \json_decode(\json_encode(\simplexml_load_string($contents, null, \LIBXML_NOCDATA)), true, \JSON_THROW_ON_ERROR);
+    }
+}
